579c2f619675ed3e733c6c39fe1f5e649cbdbf03
bilal-fazlani <bilal.m.fazlani@gmail.com>
2015-03-25 23:39:46 +0530

    fixed #19 , fixed integration tests

---
+++ TrackerEnabledDbContext.Identity.IntegrationTests/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace TrackerEnabledDbContext.Identity.IntegrationTests.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TrackerEnabledDbContext.Identity.IntegrationTests.TestTrackerIdentityContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+        }
+
+        protected override void Seed(TrackerEnabledDbContext.Identity.IntegrationTests.TestTrackerIdentityContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
