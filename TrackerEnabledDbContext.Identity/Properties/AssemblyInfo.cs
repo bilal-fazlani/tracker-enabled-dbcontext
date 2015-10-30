9cb422a4e9dd3382bc2ae0fa35eecc6e8560bb69
bilal-fazlani <bilal.m.fazlani@gmail.com>
2015-10-30 14:54:20 +0530

    updated assembly versions

--- TrackerEnabledDbContext.Identity/Properties/AssemblyInfo.cs
+++ TrackerEnabledDbContext.Identity/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.1.0.0")]
-[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyVersion("3.3.13.0")]
+[assembly: AssemblyFileVersion("3.3.13.0")]
