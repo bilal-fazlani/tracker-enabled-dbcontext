e8fc661e6d912d6105f7ed6e0dc402b3228ced12
bilal-fazlani <bilal.m.fazlani@gmail.com>
2015-11-20 00:40:27 +0530

    added missing file

---
+++ TrackerEnabledDbContext.Common.Testing/Code/RandomUtility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerEnabledDbContext.Common.Testing.Code
+{
+    public static class RandomUtility
+    {
+        public static string RandomText => Guid.NewGuid().ToString();
+
+        public static int RandomNumber => new Random().Next(100, 200);
+
+        public static DateTime RandomDate => DateTime.Now.AddDays(-RandomNumber);
+
+        public static char RandomChar
+        {
+            get
+            {
+                int num = new System.Random().Next(0, 26); // Zero to 25
+                char let = (char)('a' + num);
+                return let;
+            }
+        }
+
+        public static bool RandomBoolean => new Random().Next(2) == 0;
+    }
+}
