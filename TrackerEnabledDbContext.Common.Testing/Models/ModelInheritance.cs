9879463cac3abea486fca8af4ca13b9d7291f035
Victor Pryganov <ahahcbigeim@gmail.com>
2019-02-12 22:32:33 +0400

    #90 Fluent API for excluding fields does not work with base classes

---
+++ TrackerEnabledDbContext.Common.Testing/Models/ModelInheritance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TrackerEnabledDbContext.Common.Testing.Models
+{
+    public abstract class BaseModel
+    {
+        public int Id { get; set; }
+
+        public DateTime Modified { get; set; }
+    }
+
+    public class ExtendedModel : BaseModel
+    {
+        public string TrackedProperty { get; set; }
+    }
+}
--- TrackerEnabledDbContext.IntegrationTests/TestTrackerContext.cs
+++ TrackerEnabledDbContext.IntegrationTests/TestTrackerContext.cs
@@ -27,5 +27,6 @@ namespace TrackerEnabledDbContext.IntegrationTests
         public DbSet<TrackedModelWithCustomTableAndColumnNames> TrackedModelsWithCustomTableAndColumnNames { get; set; }
         public DbSet<SoftDeletableModel> SoftDeletableModels { get; set; }
         public DbSet<ModelWithComplexType> ModelsWithComplexType { get; set; }
+        public DbSet<ExtendedModel> ExtendedModels { get; set; }
     }
 }
