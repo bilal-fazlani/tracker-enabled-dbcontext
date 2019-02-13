49ff31b720f66612209c02b355548003abe85227
ahahcbigeim <ahahcbigeim@gmail.com>
2019-02-13 13:03:27 +0400

    #162 Multiple SELECT queries with GlobalTrackingConfig.DisconnectedContext = true (#163)
    * #162 Multiple SELECT queries with GlobalTrackingConfig.DisconnectedContext = true

---
+++ TrackerEnabledDbContext.Common/Auditors/Helpers/DbEntryValuesWrapper.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.Entity.Infrastructure;
+using TrackerEnabledDbContext.Common.Configuration;
+
+namespace TrackerEnabledDbContext.Common.Auditors.Helpers
+{
+    public class DbEntryValuesWrapper
+    {
+        protected readonly DbEntityEntry _dbEntry;
+        private DbPropertyValues _entryValues = null;
+
+        private DbPropertyValues EntryPropertyValues => _entryValues ?? (_entryValues = _dbEntry.GetDatabaseValues());
+
+        public DbEntryValuesWrapper(DbEntityEntry dbEntry)
+        {
+            _dbEntry = dbEntry;
+        }
+
+        public object OriginalValue(string propertyName)
+        {
+            object originalValue = null;
+
+            if (GlobalTrackingConfig.DisconnectedContext)
+            {
+                originalValue = EntryPropertyValues.GetValue<object>(propertyName);
+            }
+            else
+            {
+                originalValue = _dbEntry.Property(propertyName).OriginalValue;
+            }
+
+            return originalValue;
+        }
+    }
+}
